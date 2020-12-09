f478ab2c355b01b983a4fe5db0152f0ec5230f3c
Adam Ormsby <ormsbyadam@gmail.com>
2020-12-08 21:38:12 -0600

    day 8 sol 2 obscure bugs/mistakes are driving me *nuts*

--- 8/gamegear.py
+++ 8/gamegear.py
@@ -13,12 +13,17 @@ def splitCode(code):
     c = code.split(' ')
     return [c[0], c[1][0], int(c[1][1:len(c[1])])]
 
-def breakBootOnLoop(code, log={}):
-    acc = 0
-    i = 0
+def breakBootOnLoop(code):
+    log = {}
+    acc, i = 0, 0
+    steps = []
+    finished = True
 
     while i < len(code):
-        codeKey = str(i) + code[i]
+        codeKey = str(i) + '/' + code[i]
+        steps.append(codeKey)
+        # print(steps)
+
         n = log.get(codeKey)
         if n is None: n = 0
         # print(codeKey, ':', n)
@@ -26,6 +31,7 @@ def breakBootOnLoop(code, log={}):
         # if command is running a second time
         log.update({codeKey: n+1})
         if n == 1:
+            finished = False
             break
         # print(log)
 
@@ -40,13 +46,40 @@ def breakBootOnLoop(code, log={}):
         
         i += 1
         
-    return acc, log
+    return acc, log, steps, finished
+
+def fixToComplete(code, steps):
+    candidates = [x.split('/') for x in steps if 'jmp' in x or 'nop' in x]
+    # print(candidates)
+
+    acc = 0
+    for i in range(len(candidates)-1, -1, -1):
+        codeCopy = code.copy()  # fresh list
+        cur = candidates[i]
+        ind = int(candidates[i][0])
+
+        change = cur[1].replace('jmp', 'nop') if 'jmp' in cur[1] else cur[1].replace('nop', 'jmp')
+        # print(change)
+        codeCopy[ind] = change
+        # print(codeCopy)
+        acc, log, steps, fin = breakBootOnLoop(codeCopy)
+        # print(steps)
+
+        if fin:
+            # print('fin')
+            break;
+
+    return acc
 
 ################################
 
 # boot = readBootCode('test.txt')
 boot = readBootCode('boot.txt')
 # print(boot)
-acc, commandLog = breakBootOnLoop(boot)
-print([k for k in commandLog if commandLog.get(k) == 2])
+acc, commandLog, steps, fin = breakBootOnLoop(boot)
 print('acc =>', acc)
+# print(steps)
+
+
+finalAcc = fixToComplete(boot, steps)
+print('final acc =>', finalAcc)
